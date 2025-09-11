4b27bd1dba7c1b7a207cb04fdbd2377fe6559d93
Tyler Geiger <tg996676@wcupa.edu>
2025-09-11 18:29:56 -0400

    Add new status constants for resource management

--- internal/experiment/constants.go
+++ internal/experiment/constants.go
@@ -4,7 +4,11 @@ const (
 	profileName          = "cloud-edu,terraform-profile"
 	profileParamSpecJSON = "spec_json"
 
+	StatusProvisioning = "provisioning"
 	StatusProvisioned = "provisioned"
 	StatusSwapped     = "swapped"
+	StatusCreating	  = "creating"
+	StatusCreated     = "created"
+	StatusStarting 	  = "starting"
 	StatusReady       = "ready"
 )
